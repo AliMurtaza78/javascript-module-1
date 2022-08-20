e1f577deead2403e924dd8f8cc1d2d5d2b3ea0d3
Ali Murtaza <syedalim9087@gmail.com>
2022-08-20 23:11:35 +0200

    Added New code

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -1,15 +1,19 @@
 var mentor1 = "Daniel";
-var greeting = `Hello ${mentor1}`;
-console.log(greeting.toUpperCase())
 var mentor2 = "Irina";
-var greeting = `Hello ${mentor2}`;
-console.log(greeting.toUpperCase())
 var mentor3 = "Mimi";
-var greeting = `Hello ${mentor3}`;
-console.log(greeting.toUpperCase())
 var mentor4 = "Rob";
-var greeting = `Hello ${mentor4}`;
-console.log(greeting.toUpperCase())
 var mentor5 = "Yohannes";
-var greeting = `Hello ${mentor5}`;
-console.log(greeting.toUpperCase())
+
+function charUpperCase(name){
+    let mentorUpperCase = name.toUpperCase();
+    return mentorUpperCase;
+}
+
+function greetingMentors(name){
+    console.log(`HELLO ${charUpperCase(name)}`);
+}
+greetingMentors(mentor1)
+greetingMentors(mentor2)
+greetingMentors(mentor3)
+greetingMentors(mentor4)
+greetingMentors(mentor5)
