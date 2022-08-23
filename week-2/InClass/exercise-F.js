17c64d39ce8f57e684f91cc352367f8c2d01d11b
Ali Murtaza <syedalim9087@gmail.com>
2022-08-23 23:16:49 +0200

    Completed these Exercises

---
+++ week-2/InClass/exercise-F.js
@@ -0,0 +1,14 @@
+function checkingUsers(username,usertype){
+    if(usertype == "admin" || usertype == "manager"){
+        console.log("Username Valid")
+    }else if (username[0] == username[0].toUpperCase() && username.length >= 5 ){
+    console.log("Username Valid")
+    }else
+    console.log("Username Invalid")
+}
+
+
+checkingUsers("aaaaa" ,"admin");
+checkingUsers("Aaaaa" ,"manager");
+checkingUsers("aaaaa" ,"student");
+
---
+++ week-2/InClass/exercise-K.js
@@ -0,0 +1,13 @@
+const studentsOfClass = [
+    "Ali",
+    "Abu Bakar",
+    "Muazzam",
+    "Lucia",
+    "Linda",
+    "Zakria",
+  ];
+  
+  for (let i = 0; i < studentsOfClass.length; i++) {
+    const studentMessage = `Hello ${studentsOfClass[i]}`;
+    console.log(studentMessage);
+  }
